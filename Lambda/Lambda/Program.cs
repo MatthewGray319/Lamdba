66fe814e529522c0b2676794222527f1a0e60db9
Matthew Gray <matthewcharlesgray319@gmail.com>
2022-10-04 18:23:41 -0400

    Updated

---
+++ Lambda/Lambda/Program.cs
@@ -0,0 +1,13 @@
+﻿List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9};
+Func<int, bool> oddcheck = x=> x % 2 != 0;
+var oddnumbers = numbers.Where(oddcheck).ToList();
+
+
+Action<int> printOdd = x =>
+{
+    var square = x * x;
+    var cube = x * x * x;
+    Console.WriteLine($"{x} : {square}, : {cube}");
+};
+
+oddnumbers.ForEach(printOdd);
